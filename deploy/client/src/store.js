cde5de33ad07a3f162476934d3852800b3c700c9
AnasSulaiman <anasalsulaimi28@gmail.com>
2026-05-11 15:25:12 +0400

    Delete deploy directory

--- deploy/client/src/store.js
+++
@@ -1,7 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import UserReducer from './features/UserSlice';
-import PostReducer from './features/PostSlice';
- 
-export const store=configureStore({
-    reducer:{user:UserReducer,post:PostReducer}
-})
--- deploy/client/src/validations/LoginValidation.js
+++
@@ -1,6 +0,0 @@
-import * as yup from "yup";
-
-export const LoginValidation=yup.object().shape({
-    email:yup.string().required("Email is required").email("Enter valid email"),
-    password:yup.string().required("Password is required").min(6,"Password must be min 6 characters")
-});
